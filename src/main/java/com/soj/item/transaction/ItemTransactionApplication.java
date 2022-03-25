a503237d98dfdfd1fef351185a17211ab95829df
test@gmail.com <test@gmail.com>
2022-03-25 10:38:57 +0000

    first commit

--- src/main/java/com/soj/item/transaction/ItemTransactionApplication.java
+++ src/main/java/com/soj/item/transaction/ItemTransactionApplication.java
@@ -5,6 +5,7 @@ import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//first commit
 @SpringBootApplication
 public class ItemTransactionApplication {
 static final Logger logger=LogManager.getLogger(ItemTransactionApplication.class.getName());
